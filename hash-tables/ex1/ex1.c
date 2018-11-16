b7be51b3d37e287bf4d9c4eb73471e4f9dc1e6b6
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-11-16 12:58:18 -0500

    completed ex 1 and 2

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -8,6 +8,26 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
   HashTable *ht = create_hash_table(16);
 
   // YOUR CODE HERE
+  Answer *answer = malloc(sizeof(Answer));
+
+  for (int i = 0; i < length; i++) {
+    hash_table_insert(ht, weights[i], i);
+  }
+
+  for (int j = 0; j < length; j++) {
+    int index = hash_table_retrieve(ht, limit-weights[j]);
+    if (index != -1){
+      if (j > index) {
+        answer->index_1 = j;
+        answer->index_2 = index;
+      }
+      answer->index_1 = index;
+      answer->index_2 = j;
+      destroy_hash_table(ht);
+      return answer;
+    }
+  }
+
 
   return NULL;
 }
